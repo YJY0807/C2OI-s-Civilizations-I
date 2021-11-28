da791f127a90e0adc962149fc23d26a764bba258
YJY0807qwq <73064768+YJY0807qwq@users.noreply.github.com>
2021-11-28 17:08:04 +0800

    Update console.cpp

--- console.cpp
+++ console.cpp
@@ -2,11 +2,45 @@
 
 #include <console.hpp>
 #include <windows.h>
+#include <thread>
 
 using namespace std;
 
 namespace Console
 {
+    EXPORT void cInitConsole()
+    {
+        thread tHide(cHideScrollBar);
+        tHide.detach();
+
+        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+        DWORD dMode;
+        GetConsoleMode(hStdin, &dMode);
+        dMode &= ~ENABLE_QUICK_EDIT_MODE;
+        dMode &= ~ENABLE_INSERT_MODE;
+        dMode &= ~ENABLE_MOUSE_INPUT;
+        SetConsoleMode(hStdin, dMode);
+
+        HWND hCwindow = GetConsoleWindow();
+        int cx = GetSystemMetrics(SM_CXSCREEN), cy = GetSystemMetrics(SM_CYSCREEN);
+        LONG lWindow = GetWindowLong(hCwindow, GWL_STYLE);
+        SetWindowLong(hCwindow, GWL_STYLE, (lWindow | WS_POPUP | WS_MAXIMIZE) & ~WS_THICKFRAME);
+        SetWindowPos(hCwindow, HWND_TOP, 0, 0, cx, cy, 0);
+
+        cSetFont(7, 21);
+
+        SetConsoleTitle("C2OI's Civilizations I");
+    }
+
+    EXPORT void cHideScrollBar()
+    {
+        HWND hCwindow = GetConsoleWindow();
+        while (1)
+        {
+            ShowScrollBar(hCwindow, SB_BOTH, FALSE);
+        }
+    }
+
     EXPORT inline void cGotoXY(int x, int y)
     {
         HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -15,11 +49,13 @@ namespace Console
         c.Y = x;
         SetConsoleCursorPosition(hStdout, c);
     }
+
     EXPORT inline void cSetColor(int t, int b)
     {
         HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
         SetConsoleTextAttribute(hStdout, t + b * 16);
     }
+
     EXPORT inline void cSetFont(int x, int y)
     {
         CONSOLE_FONT_INFOEX cfi;
@@ -29,7 +65,40 @@ namespace Console
         cfi.dwFontSize.Y = y;
         cfi.FontWeight = FW_THIN;
         cfi.FontFamily = FF_DONTCARE;
-        wcscpy(cfi.FaceName, L"Raster");
-        SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+        wcscpy(cfi.FaceName, L"Microsoft Yahei");
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetCurrentConsoleFontEx(hStdout, FALSE, &cfi);
+    }
+
+    EXPORT inline void cSetCursor(bool b)
+    {
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cci;
+        GetConsoleCursorInfo(hStdout, &cci);
+        cci.bVisible = FALSE;
+        SetConsoleCursorInfo(hStdout, &cci);
+    }
+
+    EXPORT inline void cClearScreen()
+    {
+        COORD cScreen = {0, 0};
+        DWORD dCharwritten;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        DWORD dConsolesize;
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        GetConsoleScreenBufferInfo(hStdout, &csbi);
+        dConsolesize = csbi.dwSize.X * csbi.dwSize.Y;
+        FillConsoleOutputCharacter(hStdout, TEXT(' '), dConsolesize, cScreen, &dCharwritten);
+        GetConsoleScreenBufferInfo(hStdout, &csbi);
+        FillConsoleOutputAttribute(hStdout, csbi.wAttributes, dConsolesize, cScreen, &dCharwritten);
+        SetConsoleCursorPosition(hStdout, cScreen);
+    }
+
+    EXPORT inline int cGetScreenWidth()
+    {
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(hStdout, &csbi);
+        return csbi.dwSize.X;
     }
 }
