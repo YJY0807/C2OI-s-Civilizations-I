80cc143e8afe16782ecaab083ea9dc8e731144a3
YJY0807qwq <73064768+YJY0807qwq@users.noreply.github.com>
2021-11-28 17:08:22 +0800

    Update console.hpp

--- console.hpp
+++ console.hpp
@@ -9,14 +9,14 @@
 
 namespace Console
 {
+    EXPORT void cInitConsole();                 //Init the Console
+    EXPORT void cHideScrollBar();               //Hide the scroll bar
     EXPORT inline void cGotoXY(int x, int y);   //Move the output location
     EXPORT inline void cSetColor(int t, int b); //Set the output color, t is the text color, b is the background color
-    EXPORT inline void cSetFont(int x, int y);  //Set the console font.
-}
-
-namespace std
-{
-    using namespace Console;
+    EXPORT inline void cSetFont(int x, int y);  //Set the console font
+    EXPORT inline void cSetCursor(bool b);      //Show or hide the cursor
+    EXPORT inline void cClearScreen();          //Clear the screen
+    EXPORT inline int cGetScreenWidth();        //Get the width of console
 }
 
 #endif
