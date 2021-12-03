58f879627ecb2fd4468c858f0008d4cc2a295e97
YJY0807qwq <73064768+YJY0807qwq@users.noreply.github.com>
2021-12-03 22:40:17 +0800

    Update CCI.cpp

--- CCI.cpp
+++ CCI.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 #include <console.hpp>
-#include <windows.h>
+#include <fastout.hpp>
 #include <conio.h>
 
-using namespace std;
+using namespace Keyboard;
 using namespace Console;
-
-#define Kbhit(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1 : 0)
+using namespace FastOut;
+using namespace std;
 
 const int cA = 5;	   //Red city
 const int cB = 3;	   //Blue city
@@ -22,7 +22,9 @@ int hp[msize][msize];
 int nCount, sx, sy;							//Round accumulation, and the coordinates of the cursor
 bool bVis[msize][msize];					//bfs
 int dir[4][2] = {1, 0, -1, 0, 0, 1, 0, -1}; //bfs
-int lA, lB;									//Player level
+int lA = 1, lB;								//Player level
+int fs;										//Font size
+bool p;										//Game type
 
 //Decision information: the number at position (x, y) is moved to (x + dx, y + dy)
 struct dRet
@@ -50,33 +52,25 @@ inline void gMoveNum(int x, int y, int dx, int dy, char A);
 inline void gDiv(int dx, int dy, char A);
 inline void gDecide(char A, bool d);
 inline dRet gBFS(int x, int y, char A);
-inline void gPlus();			   //Increase the army every round, count army and land
+inline void gPlus();			   //Increases the army every round, count army and land
 inline void gChange(int x, int y); //Refresh the number of specified coordinates on the screen
 
 void gMain0();
 void gMain1();
-void gPause();
 void gEnd(); //Check the game is over
 
 int main()
 {
-	bool p;
-	int fs;
 	cInitConsole();
-	cout << "玩法 (0: 玩家 VS AI, 1: AI VS AI) : ";
+	qout << "玩法 (0: 玩家 VS AI, 1: AI VS AI) : ";
 	cin >> p;
-	cout << "字体大小: ";
+	qout << "字体大小: ";
 	cin >> fs;
 	cClearScreen();
-	cSetFont((fs / 3 != 0 ? fs / 3 : fs / 3 + 1), fs);
 	if (p)
-	{
 		gMain0();
-	}
 	else
-	{
 		gMain1();
-	}
 	return 0;
 }
 
@@ -84,62 +78,47 @@ void gMain0()
 {
 	bool cur = true;
 	int i;
-	cout << "AI A等级(终极困难请输入0): ";
+	qout << "AI A等级(终极困难请输入0): ";
 	cin >> lA;
-	cout << "AI B等级(终极困难请输入0): ";
+	qout << "AI B等级(终极困难请输入0): ";
 	cin >> lB;
 	gInit();
 	for (nCount = 0;; nCount++)
 	{
-		cGotoXY(msize + 1, 0);
+		cGotoXY(msize, 0);
 		cSetColor(15, 0);
-		cout << "第" << nCount << "回合" << endl;
+		qout << "第" << nCount << "回合" << endl;
 		if (cur)
 		{
 		Wait:
 			while (1)
 			{
-				if (Kbhit(' '))
+				if (kOnKeyDown('F'))
 				{
-					while (Kbhit(' '))
-						;
-					break;
-				}
-				if (Kbhit('F'))
-				{
-					while (Kbhit('F'))
-						;
 					cur = 0;
+					while (kOnKeyDown('F'))
+						;
 					break;
 				}
-				if (Kbhit(27))
+				if (kOnKeyDown(' '))
 				{
-					while (Kbhit(27))
-						;
-					gPause();
+					break;
 				}
 			}
 		}
 		else
 		{
-			if (Kbhit('F'))
+			if (kOnKeyDown('F'))
 			{
-				while (Kbhit('F'))
-					;
 				cur = 1;
-				goto Wait;
-			}
-			if (Kbhit(27))
-			{
+				while (kOnKeyDown('F'))
+					;
 				goto Wait;
 			}
 		}
 		if (lA == 0)
 		{
-			for (i = 1; i <= rand() % 20; i++)
-			{
-				gDecide('A', 1);
-			}
+			gDecide('A', 1);
 		}
 		for (i = 1; i <= lA; i++)
 		{
@@ -147,10 +126,7 @@ void gMain0()
 		}
 		if (lB == 0)
 		{
-			for (i = 1; i <= rand() % 20; i++)
-			{
-				gDecide('B', 1);
-			}
+			gDecide('B', 1);
 		}
 		for (i = 1; i <= lB; i++)
 		{
@@ -164,17 +140,17 @@ void gMain1()
 {
 	int in;
 	int i;
-	cout << "AI等级(终极困难请输入0): ";
+	qout << "AI等级(终极困难请输入0): ";
 	cin >> lB;
 	gInit();
 	for (nCount = 0;; nCount++)
 	{
-		cGotoXY(msize + 1, 0);
+		cGotoXY(msize, 0);
 		cSetColor(15, 0);
-		cout << "第" << nCount << "回合" << endl;
+		qout << "第" << nCount << "回合" << endl;
 		do
 		{
-		PlayerDecide:
+		Decide:
 			in = getch();
 			if (in == 224)
 			{
@@ -188,58 +164,36 @@ void gMain1()
 				if (in == 77)
 					gMove(0, 1);
 			}
-		} while (in != 'w' && in != 's' && in != 'a' && in != 'd' && in != 'z' && in != ' ' && in != 27);
-		if (in == 27)
-		{
-			while (Kbhit(27))
-				;
-			gPause();
-		}
+		} while (in != 'w' && in != 's' && in != 'a' && in != 'd' && in != 'z' && in != ' ');
 		if (in == 'w')
 		{
-			if (sx < 1)
+			if (sx < 1 || cMap[sx - 1][sy] == 'X')
 			{
-				goto PlayerDecide;
-			}
-			if (cMap[sx - 1][sy] == 'X')
-			{
-				goto PlayerDecide;
+				goto Decide;
 			}
 			gMoveNum(-1, 0, 'A');
 		}
 		if (in == 's')
 		{
-			if (sx >= msize - 1)
+			if (sx >= msize - 1 || cMap[sx + 1][sy] == 'X')
 			{
-				goto PlayerDecide;
-			}
-			if (cMap[sx + 1][sy] == 'X')
-			{
-				goto PlayerDecide;
+				goto Decide;
 			}
 			gMoveNum(1, 0, 'A');
 		}
 		if (in == 'a')
 		{
-			if (sy < 1)
+			if (sy < 1 || cMap[sx][sy - 1] == 'X')
 			{
-				goto PlayerDecide;
-			}
-			if (cMap[sx][sy - 1] == 'X')
-			{
-				goto PlayerDecide;
+				goto Decide;
 			}
 			gMoveNum(0, -1, 'A');
 		}
 		if (in == 'd')
 		{
-			if (sy >= msize - 1)
+			if (sy >= msize - 1 || cMap[sx][sy + 1] == 'X')
 			{
-				goto PlayerDecide;
-			}
-			if (cMap[sx][sy + 1] == 'X')
-			{
-				goto PlayerDecide;
+				goto Decide;
 			}
 			gMoveNum(0, 1, 'A');
 		}
@@ -247,20 +201,41 @@ void gMain1()
 		{
 			in = getch();
 			if (in == 'w')
+			{
+				if (sx < 1 || cMap[sx - 1][sy] == 'X')
+				{
+					goto Decide;
+				}
 				gDiv(-1, 0, 'A');
+			}
 			if (in == 's')
+			{
+				if (sx >= msize - 1 || cMap[sx + 1][sy] == 'X')
+				{
+					goto Decide;
+				}
 				gDiv(1, 0, 'A');
+			}
 			if (in == 'a')
+			{
+				if (sy < 1 || cMap[sx][sy - 1] == 'X')
+				{
+					goto Decide;
+				}
 				gDiv(0, -1, 'A');
+			}
 			if (in == 'd')
+			{
+				if (sy >= msize - 1 || cMap[sx][sy + 1] == 'X')
+				{
+					goto Decide;
+				}
 				gDiv(0, 1, 'A');
+			}
 		}
 		if (lB == 0)
 		{
-			for (i = 1; i <= rand() % 20; i++)
-			{
-				gDecide('B', 1);
-			}
+			gDecide('B', 1);
 		}
 		for (i = 1; i <= lB; i++)
 		{
@@ -270,37 +245,16 @@ void gMain1()
 	}
 }
 
-void gPause()
-{
-	while (Kbhit(27))
-		;
-	int i, in;
-	cClearScreen();
-	cGotoXY(0, cGetScreenWidth() / 2 - 2);
-	cout << "暂停";
-	cGotoXY(1, 0);
-	for (i = 1; i <= cGetScreenWidth(); i++)
-	{
-		cout << "-";
-	}
-	do
-	{
-		in = getch();
-	} while (in != 27);
-	while (Kbhit(27))
-		;
-	cClearScreen();
-}
-
 void gInit()
 {
 	int i, x, y;
 	string sSeed;
 	unsigned int sd = 20190622;
-	cout << "种子: ";
+	qout << "种子: ";
 	cin >> sSeed;
-	cSetCursor(0);
 	cClearScreen();
+	cSetCursor(0);
+	cSetFont(((int)(fs / 3.6) != 0 ? (int)(fs / 3.6) : (int)(fs / 3.6) + 1), fs);
 	for (i = 0; i < (int)sSeed.size(); i++)
 		sd = sd * 233 + sSeed[i];
 	srand(sd);
@@ -321,7 +275,7 @@ void gInit()
 			cMap[x][y] = 'X';
 			cGotoXY(x, y * 5);
 			cSetColor(15, 0);
-			cout << "  X";
+			qout << "  X";
 		}
 	}
 	for (i = 1; i <= grey; i++)
@@ -337,21 +291,10 @@ void gInit()
 			gChange(x, y);
 		}
 	}
-	cGotoXY(msize, 0);
-	for (i = 1; i <= msize * 5 + 2; i++)
-	{
-		cout << "-";
-	}
-	for (i = 0; i <= msize; i++)
-	{
-		cGotoXY(i, msize * 5 + 1);
-		cSetColor(7, 0);
-		cout << "|";
-	}
 	sx = sy = 0;
 	cGotoXY(0, 3);
 	cSetColor(15, 0);
-	cout << "<";
+	qout << "<";
 }
 
 inline void gMove(int dx, int dy)
@@ -362,13 +305,13 @@ inline void gMove(int dx, int dy)
 	if (!(dx >= 0 && dx < msize && dy >= 0 && dy < msize))
 		return;
 	cGotoXY(sx, sy * 5 + 4);
-	cout << " ";
+	qout << " ";
 	sx = dx;
 	sy = dy;
 	cGotoXY(sx, sy * 5 + 4);
-	cout << "<";
-	cGotoXY(msize + 2, 0);
-	cout << "拥有者: 玩家" << cMap[dx][dy] << ", 兵力: " << hp[dx][dy] << "            ";
+	qout << "<";
+	cGotoXY(msize + 1, 0);
+	qout << "拥有者: 玩家" << cMap[dx][dy] << ", 兵力: " << hp[dx][dy] << "            ";
 }
 
 inline void gMoveNum(int x, int y, int dx, int dy, char A)
@@ -437,14 +380,20 @@ inline void gDiv(int dx, int dy, char A)
 	dx += sx;
 	dy += sy;
 	if (!(dx >= 0 && dx < msize && dy >= 0 && dy < msize && cMap[dx][dy] != 'X'))
+	{
 		return;
+	}
 	char &cd = cMap[dx][dy];
 	int &hd = hp[dx][dy], &hs = hp[sx][sy], tmp = hs / 2;
-	if (tmp == 0)
+	if (tmp == 0 || cd == 'X' || hs == 1)
+	{
 		return;
-	if (cd != a && cd != A && hd >= tmp - 1)
-		return;
-	if (cd == 0)
+	}
+	if (cd != a && cd != A && hd >= hs - 1)
+	{
+		hd -= tmp - 1;
+	}
+	else if (cd == 0)
 	{
 		cd = a;
 		hd = tmp;
@@ -505,7 +454,7 @@ inline void gDecide(char A, bool d)
 					{
 						continue;
 					}
-					if ((cMap[dx][dy] == 0 || cMap[dx][dy] == b) && hp[dx][dy] / 5 * 4 < hp[i][j] - 1)
+					if ((cMap[dx][dy] == 0 || cMap[dx][dy] == b) && hp[dx][dy] < hp[i][j] - 1)
 					{
 						res.push_back({dir[k][0], dir[k][1], i, j});
 						break;
@@ -599,9 +548,14 @@ inline void gPlus()
 		{
 			for (j = 0; j < msize; j++)
 			{
-				if (cMap[i][j] == 'a' || cMap[i][j] == 'b')
+				if (cMap[i][j] == 'a')
 				{
-					++hp[i][j];
+					hp[i][j] += (lA == 0 ? 2 : 1);
+					gChange(i, j);
+				}
+				if (cMap[i][j] == 'b')
+				{
+					hp[i][j] += (lB == 0 ? 2 : 1);
 					gChange(i, j);
 				}
 			}
@@ -611,9 +565,14 @@ inline void gPlus()
 	{
 		for (j = 0; j < msize; j++)
 		{
-			if (cMap[i][j] == 'A' || cMap[i][j] == 'B')
+			if (cMap[i][j] == 'A')
 			{
-				++hp[i][j];
+				hp[i][j] += (lA == 0 ? 2 : 1);
+				gChange(i, j);
+			}
+			if (cMap[i][j] == 'B')
+			{
+				hp[i][j] += (lB == 0 ? 2 : 1);
 				gChange(i, j);
 			}
 			if (cMap[i][j] == 'A' || cMap[i][j] == 'a')
@@ -623,35 +582,26 @@ inline void gPlus()
 		}
 	}
 	cSetColor(15, 0);
-	cGotoXY(msize + 3, 0);
-	cout << "红方兵力: " << aa << "   " << endl;
-	cout << "红方土地: " << al << "   " << endl;
-	cout << "蓝方兵力: " << ba << "   " << endl;
-	cout << "蓝方土地: " << bl << "   " << endl;
+	cGotoXY(msize + 2, 0);
+	qout << "红方兵力: " << aa << "   " << endl;
+	qout << "红方土地: " << al << "   " << endl;
+	qout << "蓝方兵力: " << ba << "   " << endl;
+	qout << "蓝方土地: " << bl << "   " << endl;
 	gEnd();
 }
 
 void gEnd()
 {
+	cSetCursor(1);
 	if (cMap[0][0] == 'B')
 	{
-		for (long long i = 1; i <= 5000000000LL; i++)
-		{
-			i++;
-		}
-		cClearScreen();
-		cout << "蓝方获胜" << endl;
+		qout << "蓝方获胜" << endl;
 		system("pause > nul");
 		exit(0);
 	}
 	if (cMap[msize - 1][msize - 1] == 'A')
 	{
-		for (long long i = 1; i <= 5000000000LL; i++)
-		{
-			i++;
-		}
-		cClearScreen();
-		cout << "红方获胜" << endl;
+		qout << "红方获胜" << endl;
 		system("pause > nul");
 		exit(0);
 	}
@@ -675,23 +625,23 @@ inline void gChange(int x, int y)
 	cSetColor(15, c);
 	int val = hp[x][y];
 	if (val < 10)
-		cout << "   " << val;
+		qout << "   " << val;
 	else if (val < 100)
-		cout << "  " << val;
+		qout << "  " << val;
 	else if (val < 1000)
-		cout << " " << val;
+		qout << " " << val;
 	else if (val < 10000)
-		cout << "  " << val / 1000 << "K";
+		qout << "  " << val / 1000 << 'K';
 	else if (val < 100000)
-		cout << " " << val / 1000 << "K";
+		qout << " " << val / 1000 << 'K';
 	else if (val < 1000000)
-		cout << val / 1000 << "K";
+		qout << val / 1000 << 'K';
 	else if (val < 10000000)
-		cout << "  " << val / 1000000 << "M";
+		qout << "  " << val / 1000000 << 'M';
 	else if (val < 100000000)
-		cout << " " << val / 1000000 << "M";
+		qout << " " << val / 1000000 << 'M';
 	else if (val < 1000000000)
-		cout << val / 1000000 << "M";
+		qout << val / 1000000 << 'M';
 	else
-		cout << "INF";
+		qout << "INF";
 }
