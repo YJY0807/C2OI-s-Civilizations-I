c3fa06fff308b5796c35ce52d7db11c8b83faa2f
YJY0807qwq <73064768+YJY0807qwq@users.noreply.github.com>
2021-12-04 21:12:07 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 title Build CCI
 
+
+set /p PROCESSOR=Enter your system digit (32 or 64):
+if /i "%PROCESSOR%"=="64" ( goto :x64 )
+goto :x86
+
+:x64
 echo Your G++ Version:
 g++.exe -v
 
@@ -8,14 +14,51 @@ echo=
 echo ---Compiling start-----------------
 
 echo=
-echo Compiling console.hpp:
-echo     command: g++.exe -shared -g c:\Users\lilli\Documents\cpp\CCI\console.cpp -o c:\Users\lilli\Documents\cpp\CCI\console.dll -std=c++20 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
-g++.exe -shared -g c:\Users\lilli\Documents\cpp\CCI\console.cpp -o c:\Users\lilli\Documents\cpp\CCI\console.dll -std=c++20 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+echo Compiling conkey.cpp:
+echo     command: g++.exe -shared -g conkey.cpp -o conkey.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+g++.exe -shared -g conkey.cpp -o conkey.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+
+echo=
+echo Compiling fastout.cpp:
+echo     command: g++.exe -shared -g fastout.cpp -o fastout.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+g++.exe -shared -g fastout.cpp -o fastout.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
 
 echo=
 echo Compiling CCI.cpp:
-echo     command: g++.exe console.dll -g c:\Users\lilli\Documents\cpp\CCI\CCI.cpp -o c:\Users\lilli\Documents\cpp\CCI\CCI.exe -std=c++20 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
-g++.exe console.dll -g c:\Users\lilli\Documents\cpp\CCI\CCI.cpp -o c:\Users\lilli\Documents\cpp\CCI\CCI.exe -std=c++20 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+echo     command: g++.exe fastout.dll conkey.dll -g CCI.cpp -o CCI.exe -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+g++.exe fastout.dll conkey.dll -g CCI.cpp -o CCI.exe -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m64 -static-libgcc
+
 
 echo ---Compiling end-------------------
 pause
+goto :end
+
+
+:x86
+echo Your G++ Version:
+g++.exe -v
+
+echo=
+echo ---Compiling start-----------------
+
+echo=
+echo Compiling conkey.cpp:
+echo     command: g++.exe -shared -g conkey.cpp -o conkey.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+g++.exe -shared -g conkey.cpp -o conkey.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+
+echo=
+echo Compiling fastout.cpp:
+echo     command: g++.exe -shared -g fastout.cpp -o fastout.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+g++.exe -shared -g fastout.cpp -o fastout.dll -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+
+echo=
+echo Compiling CCI.cpp:
+echo     command: g++.exe fastout.dll conkey.dll -g CCI.cpp -o CCI.exe -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+g++.exe fastout.dll conkey.dll -g CCI.cpp -o CCI.exe -std=c++14 -Ofast -Wl,-stack=1073741824 -lm -I. -march=native -Wall -fno-asm -pipe -m32 -static-libgcc
+
+echo ---Compiling end-------------------
+pause
+goto :end
+
+:end
+start CCI.exe
